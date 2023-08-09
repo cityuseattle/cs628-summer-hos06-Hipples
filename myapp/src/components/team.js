d6f9fb3c222d025693f53ca272c2f9653dd55d4d
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-08-08 20:28:51 -0700

    section 8

--- myapp/src/components/team.js
+++ myapp/src/components/team.js
@@ -1,17 +1,28 @@
-import { Link, Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, Outlet, Navigate } from 'react-router-dom';
 
-const Team = () => (
-  <div>
-    <h2>This is Team Component</h2>
+const Team = () => {
+  const [ redirectToHome, setRedirectToHome ] = useState(false);
+
+  const handleRedirectToHome = () => setRedirectToHome(true); 
+
+  if (redirectToHome) { return <Navigate to="/" /> }
+
+  return (
     <div>
-      <ul>
-        <li><Link to="/team/member/1">Member 1</Link></li>
-        <li><Link to="/team/member/2">Member 2</Link></li>
-        <li><Link to="/team/member/3">Member 3</Link></li>
-      </ul>
+      <h2>This is Team Component</h2>
+      <div>
+        <ul>
+          <li><Link to="/team/member/1">Member 1</Link></li>
+          <li><Link to="/team/member/2">Member 2</Link></li>
+          <li><Link to="/team/member/3">Member 3</Link></li>
+        </ul>
+        <button onClick={handleRedirectToHome}>Redirect to Home</button>
+      </div>
+      <Outlet />
     </div>
-    <Outlet />
-  </div>
-);
+  );
+}
 
 export default Team;
+
